2ed40875ce5ab12e2d025d06e9f97cbc2cb091ae
EvgeniyGospk <evgeniy.gosp.univer@gmail.com>
2026-01-08 18:21:08 +0300

    chore: init expo project (typescript)

---
+++ src/app/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text style={styles.title}>ZenPulse</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
